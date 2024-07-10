ae70d1b61260a8b7cd0d1cbfd5eff4beee151fb7
Mateusz <mateusz.senkbeil@gmail.com>
2024-07-10 21:29:57 +0200

    update

--- Flashcard_main.py
+++ Flashcard_main.py
@@ -1,3 +1,22 @@
+import json
+
+class Json:
+    def __init__(self,name,data):
+        self.name = f'{name}.json'
+        self.data = data
+
+
+    def loadAllFlashcard(self):
+        pass
+
+    def updateFlashcard(self,front,last_repeats):
+        pass
+
+    def saveflashcard(self):
+        with open(self.name, 'w') as file:
+            json.dump(self.data, file, indent=4, ensure_ascii=False)
+
+
 class Flashcard:
     instances = []
     count = 0
@@ -18,9 +37,20 @@ class Flashcard:
     def get_count(cls):
         return cls.count
 
+
 F_car = Flashcard('car','samochod',[10010,12546],[{"test1":"Atest1"},{"test2":"Atest2"}])
 F_areoplane = Flashcard('areoplane','samolot',[10010,12546],[{"test1":"Atest1"},{"test2":"Atest2"}])
 
+name = "test"
+
+
+flashcards = [F_car,F_areoplane]
+
+data = {'flashcards': [flashcard.__dict__
+                       for flashcard in flashcards] }
+
+test = Json(name,data)
+test.saveflashcard()
 
 all_instances = Flashcard.get_all_instances()
 print(Flashcard.get_count())
